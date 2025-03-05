0a9e33ff7eed8381386f55f1b0433c129bc9b210
chrishuber11 <chrisjhuber11@gmail.com>
2025-03-05 12:41:10 -0500

    Update

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Talk(models.Model):
+    date = models.CharField(max_length=10)
+    title = models.CharField(max_length=20)
+    speaker = models.CharField(max_length=30)
+    
+    class Meta:
+        db_table = 'talks'
+
+    def __str__(self):
+        return self.title
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Talk
 
 def index(request):
-    return render(request, 'index.html')
+    specific_date = '3/4/2025'  # Replace with the desired date
+    talks = Talk.objects.filter(date=specific_date)
+    return render(request, 'index.html', {'talks': talks, 'specific_date': specific_date})
 
 def archive(request):
     return render(request, 'archive.html')
