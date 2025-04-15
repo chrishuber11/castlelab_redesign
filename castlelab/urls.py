cedf844e53e41aab01244938a8933c9afee013aa
chrishuber11 <chrisjhuber11@gmail.com>
2025-04-14 20:01:08 -0400

    Submit Talks feature implemented with security, css fixes for events, small html adjustments

--- castlelab/urls.py
+++ castlelab/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# Add support for static and media files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/models.py
+++ website/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Talk(models.Model):
     date = models.DateField()
-    title = models.CharField(max_length=20)
-    speaker = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
+    speaker = models.CharField(max_length=50)
     
     YES = 'Yes'
     NO = 'No'
